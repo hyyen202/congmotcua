aafd6108f4feafb87f2b9027617247e3086dbed6
hyyen202 <yentyty1405@gmail.com>
2024-02-24 21:13:09 +0700

    update logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,5 @@
-<header class="d-flex flex-wrap justify-content-center py-3 mb-4">
-  <a href="#" class="d-flex flex-column align-items-center">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Logo_ctuet.png" class="logo" alt="Logo"> 
-    <div class="text-logo mt-2 text-center">Trường Đại Học Kỹ Thuật Công Nghệ Cần Thơ</div>
+<header id="header" class="d-flex flex-lg-row flex-column justify-content-lg-start justify-content-center">
+  <a href="#" class="d-flex flex-column">
+    <span class="text-logo mt-2 ">Trường Đại Học Kỹ Thuật Công Nghệ Cần Thơ</span>
   </a>
 </header>
