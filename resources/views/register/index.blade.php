eafd9e4e037faac06060e8455e9ec391111f2158
hyyen202 <yentyty1405@gmail.com>
2024-02-27 21:48:41 +0700

    commit

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -39,8 +39,17 @@
             @include('user.infor')
         </div>
         <span> * Nếu bị sai thông tin, sinh viên vui lòng liên hệ Phòng Đào Tạo để được hỗ trợ</span>
-
-        
+        <div class="progress flex-column doc" style="width: 4px; height: 10rem; position: relative;">
+            <div class="progress-bar" role="progressbar" style="height: 15%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="label" style="top: 15%; position: absolute; left: 100%;">Label 1</div>
+          
+            <div class="progress-bar bg-success" role="progressbar" style="height: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="label" style="top: 30%; position: absolute; left: 100%;">Label 2</div>
+          
+            <div class="progress-bar bg-info" role="progressbar" style="height: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="label" style="top: 50%; position: absolute; left: 100%;">Label 3</div>
+          </div>
+          
           
         
     </div>
